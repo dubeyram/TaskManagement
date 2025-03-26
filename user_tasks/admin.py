e339e7d72ab56d4a96e660184a27c4688ea99039
Ram Gopal Dubey <rambackenddev@gmail.com>
2025-03-26 13:40:31 +0530

    Updated the admin view for the models.

--- user_tasks/admin.py
+++ user_tasks/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from user_tasks.models import Task, User
 
-# Register your models heree
-
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("id", "name", "status", "created_at", "completed_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("name",)
+    ordering = ("-created_at",)
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username",)
+    list_display = ("id", "username", "email", "date_joined", "is_active")
+    list_filter = ("is_active", "date_joined")
+    search_fields = ("username", "email")
+    ordering = ("-date_joined",)
 
 
 admin.site.register(Task, TaskAdmin)
